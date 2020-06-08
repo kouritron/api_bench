304ab9115118787d1c657179142acd179f785a41
kouritron <kouritron@users.noreply.github.com>
2020-06-08 11:35:22 -0400

    get wrk

---
+++ wrk/get_wrk.py
@@ -0,0 +1,57 @@
+# get wrk, and do a small test or show the commands for it.
+
+import os
+import sys
+import json
+import time
+import random
+import subprocess as sp
+
+from pathlib import Path
+import shutil
+
+# ======================================================================================================================
+# ======================================================================================================================
+ANSI_RED = "\u001b[31m"
+ANSI_GREEN = "\u001b[32m"
+ANSI_YELLOW = "\u001b[33m"
+ANSI_BLUE = "\u001b[34m"
+ANSI_MAGENTA = "\u001b[35m"
+ANSI_CYAN = "\u001b[36m"
+ANSI_RESET = "\u001b[0m"
+
+_SEP_LINE = ANSI_YELLOW + ('-' * 80) + ANSI_RESET
+
+# ======================================================================================================================
+# ======================================================================================================================
+
+_WRK_SETUP_CMND = r"""
+git clone https://github.com/wg/wrk .
+
+
+"""
+
+# ======================================================================================================================
+# ======================================================================================================================
+if '__main__' == __name__:
+
+    print(f"**** Running {__file__}")
+
+    repo_root = (Path(__file__) / '..' / '..').resolve()
+    print(f'repo root appears to be: {repo_root}')
+    print(_SEP_LINE)
+
+    wrk_tmp_path = Path(repo_root / 'tmp_wrk').resolve()
+
+    # rm the tmp dir if it exists and make a new one.
+    shutil.rmtree(wrk_tmp_path, ignore_errors=True)
+
+    # make a new tmp dir
+    wrk_tmp_path.mkdir(parents=True, exist_ok=True)
+
+    os.chdir(wrk_tmp_path)
+
+    exit_code = sp.call(_WRK_SETUP_CMND, shell=True)
+
+    print(_SEP_LINE)
+    print(f'exit_code: {exit_code}')
